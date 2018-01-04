3371cf51f7cb6e61e8bc33dfc10aa3614775b81d
Xiong Vang <vang0641@gmail.com>
2018-01-04 17:19:00 -0600

    init react

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	const port = ":3000"
+	const port = ":5555"
 
-	ginRouter := gin.Default()
-	ginRouter.Static("/", "./public")
-	log.Fatal(ginRouter.Run(port))
+	router := gin.Default()
+	helloGroup(router)
+	router.Static("/static", "./public/static")
+	router.StaticFile("/", "./public/index.html")
+	log.Fatal(router.Run(port))
+}
+
+func helloGroup(router *gin.Engine) {
+	hello := router.Group("/hi")
+
+	hello.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+	})
+
+	hello.GET("/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
 }
